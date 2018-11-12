71b22fcf4ae88305936f8aaa8357ca4ba2e45e95
Michal Siwek <ibah@o2.pl>
2018-11-12 19:29:48 +0100

    playing with YAML: sorting, flow styles, reading/writing

--- src/checks.py
+++ src/checks.py
@@ -63,3 +63,46 @@ with open(yaml_file, 'r') as stream:
 print(data)
 print(data_loaded)
 print(data == data_loaded) # this test failed on my computer when allow_unicode=True
+
+# example yaml file
+
+experiment_yaml_file = os.path.join(yaml_folder, 'experiment.yaml')
+
+with open(experiment_yaml_file, 'r') as stream: exp = yaml.load(stream)
+print(exp)
+
+# sorting
+exp
+exp[0]['sort']
+from sortedcontainers import SortedDict
+sorted_exp = SortedDict(exp)
+print(sorted_exp.items())
+
+# example yaml file
+
+experiment_yaml_file = os.path.join(yaml_folder, 'experiment2.yaml')
+
+with open(experiment_yaml_file, 'r') as stream: description = yaml.load(stream)
+# print(description)
+
+# the first split only
+split = description[0]['split']
+step = [*split][0] # take the first entry
+description[step][split[step]]
+
+# general split
+if ('split' in description[0]):
+    for step, dim in description[0]['split'].items():
+        print(description[step][dim])
+else:
+    print('no split')
+
+# checking different flow styles
+yaml_string = yaml.dump(description, default_flow_style=False, allow_unicode=False)
+print(yaml_string)
+
+yaml_string = yaml.dump(description, default_flow_style=True, allow_unicode=False)
+print(yaml_string)
+
+yaml_string = yaml.dump(description, allow_unicode=False) # best for readabilty
+print(yaml_string)
